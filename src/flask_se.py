510f86cfad807d5e22bcfefd08250efa0db12dac
ilya@hackerdom.ru <m6uDjk35cP2h>
2021-03-26 06:57:31 +0300

    Remove PREFIX from flask_se. Depricated.

--- src/flask_se.py
+++ src/flask_se.py
@@ -3,6 +3,7 @@
 from flask import Flask
 from flask import render_template
 from flask_frozen import Freezer
+from flask import redirect, url_for
 import sys, os
 
 app = Flask(__name__, static_url_path='', static_folder='static', template_folder='templates')
@@ -12,49 +13,47 @@ app.config['FREEZER_DESTINATION'] = '../docs'
 
 freezer = Freezer(app)
 
-URL_PREFIX = "/"
-
-@app.route(URL_PREFIX + '/')
+@app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route(URL_PREFIX + '/index.html')
+@app.route('/index.html')
 def indexhtml():
-    return render_template('index.html')
+    return redirect(url_for('index'))
 
-@app.route(URL_PREFIX + '/404.html')
+@app.route('/404.html')
 def status_404():
     return render_template('404.html')
 
-@app.route(URL_PREFIX + '/contacts.html')
+@app.route('/contacts.html')
 def contacts():
     return render_template('contacts.html')
 
-@app.route(URL_PREFIX + '/students.html')
+@app.route('/students.html')
 def students():
     return render_template('students.html')
 
-@app.route(URL_PREFIX + '/bachelor/application.html')
+@app.route('/bachelor/application.html')
 def bachelor_application():
     return render_template('bachelor_application.html')
 
-@app.route(URL_PREFIX + '/bachelor/programming-technology.html')
+@app.route('/bachelor/programming-technology.html')
 def bachelor_programming_technology():
     return render_template('bachelor_programming-technology.html')
 
-@app.route(URL_PREFIX + '/bachelor/software-engineering.html')
+@app.route('/bachelor/software-engineering.html')
 def bachelor_software_engineering():
     return render_template('bachelor_software-engineering.html')
 
-@app.route(URL_PREFIX + '/master/information-systems-administration.html')
+@app.route('/master/information-systems-administration.html')
 def master_information_systems_administration():
     return render_template('master_information-systems-administration.html')
 
-@app.route(URL_PREFIX + '/master/software-engineering.html')
+@app.route('/master/software-engineering.html')
 def master_software_engineering():
     return render_template('master_software-engineering.html')
 
-@app.route(URL_PREFIX + '/department/staff.html')
+@app.route('/department/staff.html')
 def department_staff():
     staff = [
         {"name": "Терехов Андрей Николаевич", "position": "Профессор, д.ф.-м.н., заведующий кафедрой",
@@ -116,7 +115,7 @@ def department_staff():
     ]
     return render_template('department_staff.html', staff = staff)
 
-@app.route(URL_PREFIX + '/bachelor/admission.html')
+@app.route('/bachelor/admission.html')
 def bachelor_admission():
     students = [
         {"name": "Терехов Андрей Николаевич", "position": "Студент 2-го курса, Программная инженерия",
