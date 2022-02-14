466db62069c68275e526fc5dc96b2e52440fee6b
ilya@hackerdom.ru <m6uDjk35cP2h>
2022-02-14 19:03:32 +0300

    Add days to the news ages.

--- src/flask_se_config.py
+++ src/flask_se_config.py
@@ -72,7 +72,19 @@ def get_hours_since(date):
 
 
 def plural_hours(n):
-    days = ['час', 'часа', 'часов']
+    hours = ['час', 'часа', 'часов']
+    days = ['день', 'дня', 'дней']
+
+    if (n>24):
+        n = int (n/24)
+        if n % 10 == 1 and n % 100 != 11:
+            p = 0
+        elif 2 <= n % 10 <= 4 and (n % 100 < 10 or n % 100 >= 20):
+            p = 1
+        else:
+            p = 2
+
+        return str(n) + ' ' + days[p]
 
     if (n==0):
         return 'меньше часа'
@@ -83,4 +95,4 @@ def plural_hours(n):
     else:
         p = 2
 
-    return str(n) + ' ' + days[p]
+    return str(n) + ' ' + hours[p]
