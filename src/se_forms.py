3d8fad9a1e433f723bb9ed23e81227ad6ab636ef
ilya@hackerdom.ru <m6uDjk35cP2h>
2022-02-16 19:52:37 +0300

    Fix styles.

--- src/se_forms.py
+++ src/se_forms.py
@@ -21,8 +21,8 @@ class MultiCheckboxField(SelectMultipleField):
 
 class UserAddTheme(FlaskForm):
     title = StringField('title', description='Например, реализация алгоритма контекстно-свободной достижимости на OpenCL', validators=[DataRequired()])
-    description = StringField('description', description='Например, необходимо адаптировать библиотеку CLSparse для работы с булевыми матрицами и реализовать алгоритм поиска путей с КС ограничениями, используя результаты адаптации. Провести сравнение  с аналогами на существующих данных, проанализаировать результаты', widget=TextArea())
-    requirements = StringField('description', description='Например, умение программироавть на OpenCL C. Умение читать и понимать код на OpenCL C.', widget=TextArea())
+    description = StringField('description', description='Например, необходимо адаптировать библиотеку CLSparse для работы с булевыми матрицами и реализовать алгоритм поиска путей с КС ограничениями, используя результаты адаптации. Провести сравнение с аналогами на существующих данных, проанализировать результаты.', widget=TextArea())
+    requirements = StringField('description', description='Например, умение программировать на OpenCL C. Умение читать и понимать код на OpenCL C.', widget=TextArea())
     levels = MultiCheckboxField('levels', coerce=int)
     consultant = StringField('consultant')
     company = SelectField('company', choices=[])
