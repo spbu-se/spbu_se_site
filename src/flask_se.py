34c9269a82a37cb2a0abfed2cfc9023d8eced753
kleo-53 <liza.1610@mail.ru>
2022-07-28 15:58:54 +0400

    add profile page to account, add users database

--- src/flask_se.py
+++ src/flask_se.py
@@ -34,7 +34,7 @@ from flask_se_review import submit_thesis_on_review, thesis_review_index, edit_t
 from flask_se_internships import add_internship, internships_index, page_internship, delete_internship, \
     update_internship, fetch_internships
 from se_sendmail import notification_send_mail
-
+from flask_se_account import account_profile
 
 
 app = Flask(__name__, static_url_path='', static_folder='static', template_folder='templates')
@@ -141,6 +141,8 @@ app.add_url_rule('/internships/<int:id>/delete', view_func=delete_internship)
 app.add_url_rule('/internships/<int:id>/update', methods=['GET', 'POST'], view_func=update_internship)
 app.add_url_rule('/internships/fetch_internships', methods=['GET'], view_func=fetch_internships)
 
+# Account
+app.add_url_rule('/account/profile', methods=['GET', 'POST'], view_func=account_profile)
 
 # Init Database
 db.app = app
---
+++ src/flask_se_account.py
@@ -0,0 +1,237 @@
+# -*- coding: utf-8 -*-
+
+
+from flask import flash, redirect, request, render_template, url_for
+from flask_login import current_user
+
+from flask_se_auth import login_required
+from se_internship_forms import AddInternship, InternshipsFilter
+from se_models import db, Internships, InternshipFormat, InternshipCompany
+
+
+@login_required
+def account_profile():
+    user = current_user
+    return render_template('account/profile.html', user=user)
+
+
+'''
+# Base model view with access and inaccess methods
+class SeAdminModelView(ModelView):
+
+    def is_accessible(self):
+        if current_user.is_authenticated:
+            if current_user.role >= ADMIN_ROLE_LEVEL:
+                return True
+        else:
+            return False
+
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for('login_index'))
+
+
+class SeAdminModelViewThesis(SeAdminModelView):
+    pass
+
+
+class SeAdminModelViewReviewer(SeAdminModelView):
+    pass
+
+
+class SeAdminIndexView(AdminIndexView):
+
+    @expose('/')
+    def index(self):
+        thesis_key = SECRET_KEY_THESIS
+        return self.render('admin/index.html', thesis_key=thesis_key)
+
+    def is_accessible(self):
+        if current_user.is_authenticated:
+            if current_user.role >= THESIS_ROLE_LEVEL:
+                return True
+        else:
+            return False
+
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for('login_index'))
+
+
+class SeAdminModelViewUsers(SeAdminModelView):
+
+    column_exclude_list = ['password_hash']
+
+    pass
+
+
+class SeAdminModelViewSummerSchool(SeAdminModelView):
+
+    form_overrides = {
+        'description': TextAreaField,
+        'repo': TextAreaField,
+        'demos': TextAreaField
+    }
+
+    form_widget_args = {
+        'description': {
+            'rows': 10,
+            'style': 'font-family: monospace; width: 680px;'
+        },
+        'project_name': {
+            'style': 'width: 680px;'
+        },
+        'tech': {
+            'rows': 3,
+            'style': 'font-family: monospace; width: 680px;'
+        },
+        'repo': {
+            'rows': 3,
+            'style': 'font-family: monospace; width: 680px;'
+        },
+        'demos': {
+            'rows': 3,
+            'style': 'font-family: monospace; width: 680px;'
+        },
+        'advisors': {
+            'rows': 2,
+            'style': 'font-family: monospace; width: 680px;'
+        },
+        'requirements': {
+            'rows': 3,
+            'style': 'font-family: monospace; width: 680px;'
+        },
+    }
+
+    pass
+
+
+class SeAdminModelViewStaff(SeAdminModelView):
+
+    form_choices = {
+        'science_degree': [
+            ('', ''),
+            ('д.ф.-м.н.', 'д.ф.-м.н.'),
+            ('д.т.н.', 'д.т.н.'),
+            ('к.ф.-м.н.', 'к.ф.-м.н.'),
+            ('к.т.н.', 'к.т.н.')
+        ]
+    }
+
+    column_exclude_list = ['supervisor', 'adviser']
+    form_excluded_columns = ['supervisor', 'adviser']
+
+    pass
+
+
+class SeAdminModelViewNews(SeAdminModelView):
+
+    pass
+
+
+class SeAdminModelViewDiplomaThemes(SeAdminModelView):
+
+    column_labels = dict(supervisor_thesis='Научный руководитель ВКР',
+                         supervisor="Научный руководитель учебных практик",
+                         comment="Комментарий (что необходимо исправить)",
+                         status="Статус темы",
+                         requirements="Требования к студенту",
+                         title='Название темы',
+                         description='Описание темы',
+                         company='Кто представляет тему',
+                         levels='Уровень темы',
+                         consultant="Консультант",
+                         author="Автор темы (кто предложил)")
+
+    form_overrides = {
+        'description': TextAreaField,
+        'requirements': TextAreaField,
+        'comment': TextAreaField
+    }
+
+    form_widget_args = {
+        'description': {
+            'rows': 10,
+            'style': 'width: 100%;'
+        },
+        'comment': {
+            'rows': 4,
+            'style': 'width: 100%;'
+        },
+        'requirements': {
+            'rows': 4,
+            'style': 'width: 100%;'
+        }
+    }
+
+
+    pass
+
+
+class SeAdminModelViewReviewDiplomaThemes(SeAdminModelView):
+
+    can_delete = False
+    column_list = ('status', 'comment', 'title', 'description', 'requirements', 'levels', 'company')
+    column_labels = dict(supervisor_thesis='Научный руководитель ВКР',
+                         supervisor="Научный руководитель учебных практик",
+                         comment="Комментарий (что нужно исправить)",
+                         status="Статус темы",
+                         requirements="Требования к студенту",
+                         title='Название темы',
+                         description='Описание темы',
+                         company='Кто представляет тему',
+                         levels='Уровень темы',
+                         consultant="Консультант",
+                         author="Автор темы (кто предложил)")
+
+    form_overrides = {
+        'description': TextAreaField,
+        'requirements': TextAreaField,
+        'comment': TextAreaField,
+        'status': SelectField
+    }
+
+    form_args = dict(
+        status=dict(choices=[(0, 'На проверке'), (1, 'Требуется доработка'), (2, 'Одобрена')], coerce=int)
+    )
+
+    column_choices = {'status': [
+        (0, 'На проверке'),
+        (1, 'Требуется доработка'),
+        (2, 'Одобрена')
+    ]}
+
+    form_widget_args = {
+        'description': {
+            'rows': 10,
+            'style': 'width: 100%;',
+            'readonly': True
+        },
+        'requirements': {
+            'rows': 3,
+            'style': 'width: 100%;',
+            'readonly': True
+        },
+        'title': {
+            'readonly': True
+        },
+        'level': {
+            'disabled': True
+        },
+        'company': {
+            'disabled': True
+        },
+        'author': {
+            'readonly': True
+        },
+        'comment': {
+            'rows': 5,
+            'style': 'width: 100%;',
+        }
+    }
+
+    def get_query(self):
+        return self.session.query(self.model).filter(self.model.status < 2)
+
+    def get_count_query(self):
+        return self.session.query(db.func.count('*')).filter(self.model.status < 2)
+
+    pass'''
