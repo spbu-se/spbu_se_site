421da1dd458c74002a27c1559a134217cba32483
ASlugin <slugin.alexander@gmail.com>
2022-10-18 18:23:36 +0300

    Fix

--- src/flask_se_writing_thesis_staff.py
+++ src/flask_se_writing_thesis_staff.py
@@ -20,6 +20,7 @@ def writing_thesis_index():
     current_thesises = CurrentThesis.query.filter_by(supervisor_id=user_staff.id).all()
     return render_template('account/current_thesises_staff.html', thesises=current_thesises)
 
+
 @login_required
 def writing_thesis_thesis():
     user_staff = Staff.query.filter_by(user_id=current_user.id).first()
@@ -41,15 +42,15 @@ def writing_thesis_thesis():
 def writing_thesis_reports():
     user_staff = Staff.query.filter_by(user_id=current_user.id).first()
     if not user_staff:
-        return redirect(url_for('account_index'))
+        return redirect(url_for('writing_thesis_thesis'))
 
     current_thesis_id = request.args.get('id', type=int)
     if not current_thesis_id:
-        return redirect(url_for('account_index'))
+        return redirect(url_for('writing_thesis_thesis'))
 
     current_thesis = CurrentThesis.query.filter_by(supervisor_id=user_staff.id).filter_by(id=current_thesis_id).first()
     if not current_thesis:
-        return redirect(url_for('account_index'))
+        return redirect(url_for('writing_thesis_thesis'))
 
     current_report_id = request.args.get('report_id', type=int)
     reports = ThesisReport.query.filter_by(current_thesis_id=current_thesis_id).order_by(desc(ThesisReport.time)).all()
@@ -79,6 +80,4 @@ def writing_thesis_reports():
                                         form=add_report_comment)
 
     return render_template('account/reports_staff.html', thesis=current_thesis, reports=reports,
-                    form=add_report_comment)
-
-
+                           form=add_report_comment)
