0e71de5e06b3407564ba149c04b9f887b33bcb52
ASlugin <slugin.alexander@gmail.com>
2023-02-14 01:08:32 +0300

    Refactor some functions, add enumaration of templates

---
+++ src/templates/practice/admin/templates.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class PracticeAdminTemplates(Enum):
+    CURRENT_THESISES = "practice/admin/current_thesises_admin.html"
+    DEADLINE = "practice/admin/deadline_admin.html"
