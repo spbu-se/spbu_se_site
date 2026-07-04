fce6bc4ec41d161c33d38d1cbde3a38786c635fd
iakov <iakov.kirilenko@gmail.com>
2026-07-04 17:46:22 +0300

    test: add 15 route tests for auth, news, theses, diplomas, internships

--- tests/test_auth_views.py
+++ tests/test_auth_views.py
@@ -6,6 +6,9 @@ class TestAuth:
     def test_login_form_renders(self, seeded_client):
         assert_ok(seeded_client, "/login.html")
 
+    def test_upload_avatar_redirects(self, seeded_client):
+        assert_ok(seeded_client, "/upload_avatar", code={200, 302})
+
     def test_login_form_accepts_submission(self, seeded_client):
         resp = seeded_client.post("/login.html", data={"email": "test@spbu.ru", "password": "test"})
         assert resp.status_code in (200, 302)
@@ -43,6 +46,15 @@ class TestNews:
     def test_news_item_no_id(self, seeded_client):
         assert_ok(seeded_client, "/news/item.html", code={200, 302})
 
+    def test_news_submit_redirects_when_unauth(self, seeded_client):
+        assert_ok(seeded_client, "/news/submit.html", code={200, 302})
+
+    def test_news_post_vote_redirects_when_unauth(self, seeded_client):
+        assert_ok(seeded_client, "/news/post_vote", code={200, 302})
+
+    def test_news_delete_redirects_when_unauth(self, seeded_client):
+        assert_ok(seeded_client, "/news/delete", code={200, 302})
+
 
 class TestTheses:
     def test_theses_search_loads(self, seeded_client):
@@ -63,6 +75,15 @@ class TestTheses:
     def test_theses_post_form(self, seeded_client):
         assert_ok_or_redirect(seeded_client, "/post_theses")
 
+    def test_theses_download_nonexistent(self, seeded_client):
+        assert_ok(seeded_client, "/thesis_download", code={200, 302})
+
+    def test_theses_delete_tmp(self, seeded_client):
+        assert_ok(seeded_client, "/theses_delete_tmp", code={200, 302})
+
+    def test_theses_add_tmp(self, seeded_client):
+        assert_ok(seeded_client, "/theses_add_tmp", code={200, 302})
+
 
 class TestInternships:
     def test_internships_index(self, seeded_client):
@@ -76,6 +97,15 @@ class TestInternships:
 
     def test_internship_add_form(self, seeded_client):
         assert_ok_or_redirect(seeded_client, "/internships/add")
+
+    def test_internship_detail_with_id(self, seeded_client):
+        assert_ok(seeded_client, "/internships/1", code={200, 302, 404})
+
+    def test_internship_delete(self, seeded_client):
+        assert_ok(seeded_client, "/internships/1/delete", code={200, 302, 404})
+
+    def test_internship_update_redirects(self, seeded_client):
+        assert_ok(seeded_client, "/internships/1/update", code={200, 302, 404})
 
 
 class TestDiplomas:
@@ -97,6 +127,18 @@ class TestDiplomas:
     def test_diplomas_user_themes(self, seeded_client):
         assert_ok_or_redirect(seeded_client, "/diplomas/user_themes.html")
 
+    def test_diplomas_delete_theme_redirects(self, seeded_client):
+        assert_ok(seeded_client, "/diplomas/delete_theme.html", code={200, 302})
+
+    def test_diplomas_edit_theme_redirects(self, seeded_client):
+        assert_ok(seeded_client, "/diplomas/edit_theme.html", code={200, 302})
+
+    def test_diplomas_archive_theme_redirects(self, seeded_client):
+        assert_ok(seeded_client, "/diplomas/archive_theme", code={200, 302})
+
+    def test_diplomas_unarchive_theme_redirects(self, seeded_client):
+        assert_ok(seeded_client, "/diplomas/unarchive_theme", code={200, 302})
+
 
 class TestPractice:
     def test_practice_guide(self, seeded_client):
@@ -116,20 +158,26 @@ class TestSummerSchools:
 
 
 class TestBachelor:
-    @pytest.mark.parametrize("path", [
-        "/bachelor/application.html",
-        "/bachelor/admission.html",
-        "/bachelor/programming-technology.html",
-        "/bachelor/software-engineering.html",
-    ])
+    @pytest.mark.parametrize(
+        "path",
+        [
+            "/bachelor/application.html",
+            "/bachelor/admission.html",
+            "/bachelor/programming-technology.html",
+            "/bachelor/software-engineering.html",
+        ],
+    )
     def test_bachelor_pages(self, seeded_client, path):
         assert_ok(seeded_client, path)
 
 
 class TestErrorHandlers:
-    @pytest.mark.parametrize("path", [
-        "/this-route-does-not-exist-at-all",
-        "/nonexistent.html",
-    ])
+    @pytest.mark.parametrize(
+        "path",
+        [
+            "/this-route-does-not-exist-at-all",
+            "/nonexistent.html",
+        ],
+    )
     def test_404_pages(self, seeded_client, path):
         assert_ok(seeded_client, path, code=404)
